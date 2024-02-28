94dc10ca265443e1b6d23b88dcd08f95cff63ca2
sameer rasa <crazzywizard@gmail.com>
2024-02-27 18:24:44 -0700

    added chainId

--- src/EventHandlers.ts
+++ src/EventHandlers.ts
@@ -2,47 +2,44 @@
  *Please refer to https://docs.envio.dev for a thorough guide on all Envio indexer features*
  */
 import {
-    SoundCreatorV2Contract_Created_loader,
-    SoundCreatorV2Contract_Created_handler,
-} from "../generated/src/Handlers.gen";
+  SoundCreatorV2Contract_Created_loader,
+  SoundCreatorV2Contract_Created_handler
+} from '../generated/src/Handlers.gen';
 
-import {
-    SoundCreatorV2_CreatedEntity,
-EventsSummaryEntity
-} from "../generated/src/Types.gen";
+import { SoundCreatorV2_CreatedEntity, EventsSummaryEntity } from '../generated/src/Types.gen';
 
-export const GLOBAL_EVENTS_SUMMARY_KEY = "GlobalEventsSummary";
+export const GLOBAL_EVENTS_SUMMARY_KEY = 'GlobalEventsSummary';
 
 const INITIAL_EVENTS_SUMMARY: EventsSummaryEntity = {
   id: GLOBAL_EVENTS_SUMMARY_KEY,
-    soundCreatorV2_CreatedCount: BigInt(0),
+  soundCreatorV2_CreatedCount: BigInt(0)
 };
 
-    SoundCreatorV2Contract_Created_loader(({ event, context }) => {
+SoundCreatorV2Contract_Created_loader(({ event, context }) => {
   context.EventsSummary.load(GLOBAL_EVENTS_SUMMARY_KEY);
 });
 
-    SoundCreatorV2Contract_Created_handler(({ event, context }) => {
+SoundCreatorV2Contract_Created_handler(({ event, context }) => {
   const summary = context.EventsSummary.get(GLOBAL_EVENTS_SUMMARY_KEY);
 
-  const currentSummaryEntity: EventsSummaryEntity =
-    summary ?? INITIAL_EVENTS_SUMMARY;
+  const currentSummaryEntity: EventsSummaryEntity = summary ?? INITIAL_EVENTS_SUMMARY;
 
   const nextSummaryEntity = {
     ...currentSummaryEntity,
-    soundCreatorV2_CreatedCount: currentSummaryEntity.soundCreatorV2_CreatedCount + BigInt(1),
+    soundCreatorV2_CreatedCount: currentSummaryEntity.soundCreatorV2_CreatedCount + BigInt(1)
   };
 
   const soundCreatorV2_CreatedEntity: SoundCreatorV2_CreatedEntity = {
     id: event.transactionHash + event.logIndex.toString(),
-      implementation: event.params.implementation      ,
-      edition: event.params.edition      ,
-      owner: event.params.owner      ,
-      initData: event.params.initData      ,
-      contracts: event.params.contracts      ,
-      data: event.params.data      ,
-      results: event.params.results      ,
-    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY,
+    implementation: event.params.implementation,
+    edition: event.params.edition,
+    owner: event.params.owner,
+    initData: event.params.initData,
+    contracts: event.params.contracts,
+    data: event.params.data,
+    results: event.params.results,
+    chainId: event.chainId as any as bigint,
+    eventsSummary: GLOBAL_EVENTS_SUMMARY_KEY
   };
 
   context.EventsSummary.set(nextSummaryEntity);
